2af47a044d0680ef9708b4bfbd7f985282fbed10
Zachary Esco <141782310+z-esco@users.noreply.github.com>
2024-11-20 13:19:48 -0500

    Update connectionChecker.py
    Added documentation to help reader understand usability

--- connectionChecker.py
+++ connectionChecker.py
@@ -1,9 +1,9 @@
+#In this program, go to the line that begins with link and fill in the appropriate info
+#This program will attempt to connnect to a SQL database from your local system
+#The program will return proper error messages based on the issue to help solve it
+
 import mysql.connector
 from mysql.connector import errorcode
-
-
-
-
 
 try:
   link = mysql.connector.connect(user = '', password = '', host = '' database = '', auth_plugin='')
