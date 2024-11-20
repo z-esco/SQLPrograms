1ba5842370ad6745703edd7d77d7243cfa8e0975
Zachary Esco <141782310+z-esco@users.noreply.github.com>
2024-11-20 13:54:58 -0500

    Update roundsInfoLoader.py
    Added more detailed documentation

--- roundsInfoLoader.py
+++ roundsInfoLoader.py
@@ -1,8 +1,16 @@
+#This file writes and executes querioes to add entries to a database.
+#The program expects to read files with a certain structure
+#First, the SQL table must be written in the 'tableName' 
+#Second a txt file containing the data to be uploaded should be inputed in nthe "filename" variable
+#In this file, each line should have a "number" (or address) followed by a lettert
+#In my uses, the nunber refered to an apartment number and the letter was that aapartments response to a sales pitch
+
+
 import mysql.connector
 
 #global variables
-tablename = "vox"
-filename = "vox.txt"
+tablename = ""
+filename = ""
 
 #create connection
 link = mysql.connector.connect(user = '', password = '', host = '', database = '', auth_plugin='')
